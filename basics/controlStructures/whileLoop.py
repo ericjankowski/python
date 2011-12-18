928a5110ce8811e918d4c50a7326b714ddec8bcc
Eric Jankowski <eric.r.jankowski@gmail.com>
2011-12-18 13:25:53 -0600

    simple while loop over a list

---
+++ basics/controlStructures/whileLoop.py
@@ -0,0 +1,6 @@
+herbie_hancock_tunes = ["Maiden Voyage", "The Eye Of The Hurricane", "Little One", "Survival Of The Fittest", "Dolphin Dance"]
+
+count=0
+while count < len(herbie_hancock_tunes):
+	print(herbie_hancock_tunes[count])
+	count = count + 1
